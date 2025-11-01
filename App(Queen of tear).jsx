54360a09dc68352f0ea0bb9f06a0337efb7a8784
Dimple Parihar <122507270+Dimple090@users.noreply.github.com>
2025-11-01 20:45:24 +0530

    Add files via upload

---
+++ App(Queen of tear).jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+export default function Netflix() {
+  return (
+    
+    <div>
+        <div>
+            <img src="kim-soo-hyun-queen-of-tears-scaled " alt='queen of tears' width="40%" height="40%"></img>
+        </div>
+       <h2>Name: Queen of Tears</h2>
+       <h3>Rating:8.2</h3>
+       <p>Summary:Queen of Tears is a romantic drama about Baek Hyun-woo, a humble lawyer from a small town, and Hong Hae-in, a rich and proud chaebol heiress.
+
+They are married but on the verge of divorce — until Hae-in is diagnosed with a terminal illness. Facing this tragedy, the couple rediscovers their love, heals old wounds, and stands together against family conflicts and corporate power struggles.</p>
+    </div>
+  );
+}
+
